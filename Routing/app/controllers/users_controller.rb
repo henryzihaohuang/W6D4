3a7ace41744a2504fcab8ce3d61fd266aa67b74d
russelkamal <russelhkamal@gmail.com>
2020-12-03 14:12:20 -0500

    first

--- Routing/app/controllers/users_controller.rb
+++ Routing/app/controllers/users_controller.rb
@@ -1,3 +1,15 @@
-class UsersControllers < ApplicationController 
-    
+class UsersController < ApplicationController 
+    def index
+        #users = User.all
+        render plain: "I'm in the index action!"
+    end
+
+    def create
+        render json: params
+    end
+
+    def show
+        #user = User.find(params[:id])
+        render json: params
+    end
 end
--- Routing/config/routes.rb
+++ Routing/config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-# resources :users
+resources :users
 
-get 'users/new', to: 'users#new', as: 'new_user'
-get "/users", to: "users#index"
-get "/users/:id", to: "users#show"
-post "/users", to: "users#create"
-patch "/posts/:id", to: "posts#update"
-put "/posts/:id", to: "posts#update"
-delete "/posts/:id", to: "posts#destroy"
+# get "/users", to: "users#index"
+# get "/users/:id", to: "users#show"
+# post "/users", to: "users#create"
+# patch "/users/:id", to: "users#update"
+# put "/users/:id", to: "users#update"
+# delete "/users/:id", to: "users#destroy"
 
 
 end
